b660e957032bfc7f65d1e64b1f07e08588d9577d
Unknown <matthias.zunhammer@uk-essen.de>
2017-01-31 10:35:36 +0100

    Try out Color Brewer Colors

--- D_Exploratory_Analyses/A_Check_Image_Alignment/A_Check_Coverage.m
+++ D_Exploratory_Analyses/A_Check_Image_Alignment/A_Check_Coverage.m
@@ -32,10 +32,12 @@ volume_coverage(rfilenames,outfile)
 %t='/Users/matthiaszunhammer/Documents/MATLAB/spm12/canonical/single_subj_T1.nii';
 t='/Users/matthiaszunhammer/Documents/MATLAB/spm12/canonical/fsl_standards/MNI152_T1_1mm_brain.nii'
 
+%colorused=brighten(flipud(cbrewer('seq','OrRd',1000)),0) % Try out cbrewer
+%colors
 [~,fname,~]=fileparts(outfile);
 plot_nii_results(t,...
                  'overlays',{outfile},... %Cell array of overlay volumes
-                 'ov_color',{brighten(parula,-0.75)},... %Cell array of overlay colors (RGB)
+                 'ov_color',{brighten(parula,-0.75)},... %Cell array of overlay colors (RGB)    
                  'ov_transparency',[0.6],...
                  'slices',[0,0,0],... %Cell array of slices (MNI)
                  'slice_ori',[1,2,3],...
