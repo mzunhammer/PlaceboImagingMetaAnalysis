cb6dc9459b65e26346d41477081ca62392d2a295
Unknown <matthias.zunhammer@uk-essen.de>
2017-05-18 17:11:40 +0200

    Change pathnames produced by Image/Voxel_Size_Equalizer

--- B_Prepro_Images/A_Equalize_Image_Size_and_Mask.m
+++ B_Prepro_Images/A_Equalize_Image_Size_and_Mask.m
@@ -19,22 +19,22 @@ spm_write_vol(outheader,mask);
 
 df.norm_img=cell(size(df.img));
 for i=1:length(df.img)
-infile=fullfile(datapath,df.img{i});
-[path,filename,ext]=fileparts(infile);
-df.norm_img{i}=fullfile(path,['r_',filename,ext]);
-disp(['Masking ',df.norm_img{i}])    
-matlabbatch{1}.spm.util.imcalc.input = {
-                                        maskpath
-                                        infile
-                                        };
-matlabbatch{1}.spm.util.imcalc.output = df.norm_img{i};
-matlabbatch{1}.spm.util.imcalc.outdir = {};
-matlabbatch{1}.spm.util.imcalc.expression = 'logical(i1).*i2';
-matlabbatch{1}.spm.util.imcalc.var = struct('name', {}, 'value', {});
-matlabbatch{1}.spm.util.imcalc.options.dmtx = 0;
-matlabbatch{1}.spm.util.imcalc.options.mask = 0;
-matlabbatch{1}.spm.util.imcalc.options.interp = 1;
-matlabbatch{1}.spm.util.imcalc.options.dtype = 4;
-spm_jobman('run', matlabbatch);
+    infile=fullfile(datapath,df.img{i});
+    [path,filename,ext]=fileparts(df.img{i});
+    df.norm_img{i}=fullfile(path,['r_',filename,ext]);
+    disp(['Masking ',df.norm_img{i}])    
+    matlabbatch{1}.spm.util.imcalc.input = {
+                                            maskpath
+                                            infile
+                                            };
+    matlabbatch{1}.spm.util.imcalc.output = fullfile(datapath,df.norm_img{i});
+    matlabbatch{1}.spm.util.imcalc.outdir = {};
+    matlabbatch{1}.spm.util.imcalc.expression = 'logical(i1).*i2';
+    matlabbatch{1}.spm.util.imcalc.var = struct('name', {}, 'value', {});
+    matlabbatch{1}.spm.util.imcalc.options.dmtx = 0;
+    matlabbatch{1}.spm.util.imcalc.options.mask = 0;
+    matlabbatch{1}.spm.util.imcalc.options.interp = 1;
+    matlabbatch{1}.spm.util.imcalc.options.dtype = 4;
+    spm_jobman('run', matlabbatch);
 end
 save(fullfile(datapath,'AllData.mat'),'df');
