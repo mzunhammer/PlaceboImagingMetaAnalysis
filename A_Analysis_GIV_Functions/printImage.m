10d5506016b2d263c369435c278c5220adc8338c
Unknown <matthias.zunhammer@uk-essen.de>
2017-07-01 13:28:22 +0200

    Print neg +pos nii results as fsl cluster requires it

--- A_Analysis_GIV_Functions/printImage.m
+++ A_Analysis_GIV_Functions/printImage.m
@@ -16,24 +16,25 @@ outheader=rmfield(outheader,'pinfo'); %remove pinfo otherwise there may be scali
 outheader.descrip='spm - algebra';
 spm_write_vol(outheader,outImg);
 
-% Print Pain positive effects only
-outImg(masking)=masked_stats;
-outImg(outImg<0)=0;
-outpath=fullfile([outbasename,'_pos.nii']);
-outheader=maskheader;
-outheader.fname=outpath;
-outheader.dt=[4,0]; %data_type (see: spm_type) should be at least int16 to allow for negative values
-outheader=rmfield(outheader,'pinfo'); %remove pinfo otherwise there may be scaling problems with the data
-spm_write_vol(outheader,outImg);
+if any(sum(masked_stats<0)) %if the picture contains positive and negative values
+    % Print Pain positive effects only
+    outImg(masking)=masked_stats;
+    outImg(outImg<0)=0;
+    outpath=fullfile([outbasename,'_pos.nii']);
+    outheader=maskheader;
+    outheader.fname=outpath;
+    outheader.dt=[4,0]; %data_type (see: spm_type) should be at least int16 to allow for negative values
+    outheader=rmfield(outheader,'pinfo'); %remove pinfo otherwise there may be scaling problems with the data
+    spm_write_vol(outheader,outImg);
 
-% Print Pain negative effects only
-outImg(masking)=masked_stats.*-1;
-outImg(outImg<=0)=0;
-outpath=fullfile([outbasename,'_neg.nii']);
-outheader=maskheader;
-outheader.fname=outpath;
-outheader.dt=[4,0]; %data_type (see: spm_type) should be at least int16 to allow for negative values
-outheader=rmfield(outheader,'pinfo'); %remove pinfo otherwise there may be scaling problems with the data
-spm_write_vol(outheader,outImg);
-
+    % Print Pain negative effects only
+    outImg(masking)=masked_stats.*-1;
+    outImg(outImg<=0)=0;
+    outpath=fullfile([outbasename,'_neg.nii']);
+    outheader=maskheader;
+    outheader.fname=outpath;
+    outheader.dt=[4,0]; %data_type (see: spm_type) should be at least int16 to allow for negative values
+    outheader=rmfield(outheader,'pinfo'); %remove pinfo otherwise there may be scaling problems with the data
+    spm_write_vol(outheader,outImg);
+end
 end
