db9009d25871c93351316cd094a9b44e12c46ecf
Unknown <matthias.zunhammer@uk-essen.de>
2017-08-21 16:49:09 +0200

    Beatyfy withinMetastats

--- A_Analysis_GIV_Functions/withinMetastats.m
+++ A_Analysis_GIV_Functions/withinMetastats.m
@@ -21,6 +21,8 @@ function StudyStat=withinMetastats(cond1,cond2)
 %d, se_d: Standardized mean difference (COHEN's d), standardized standard error
 %g, se_g: Standardized mean difference (HEDGE's g), standardized standard error
 
+%% Pre-process and check input
+
 % For convenience, if cond1 or cond2 were entered as a table, convert to arrays 
 if istable(cond1)
 cond1=cond1{:,:};
@@ -30,102 +32,85 @@ if istable(cond2)
 cond2=cond2{:,:};
 end
 
-% If cond1 or cond2 contain merely NaN, return a struct of NaNs
-if (isscalar(cond1)&&isscalar(cond1))&&(isnan(cond1)||isnan(cond2))
-    StudyStat.mu=NaN;
-    StudyStat.sd_diff=NaN;
-    StudyStat.sd_pooled=NaN;
-    StudyStat.se_mu=NaN;
-    StudyStat.n=NaN;
-    StudyStat.r=NaN;
-    StudyStat.d=NaN;
-    StudyStat.se_d=NaN;
-    StudyStat.g=NaN;
-    StudyStat.se_g=NaN;
-    StudyStat.delta=[];
-    StudyStat.std_delta=[];
-    StudyStat.r_external=[];
-    StudyStat.n_r_external=[];
-    return
-end
-
 % VERSION A: COMPUTE WHEN TWO ARRAYS (= measurement timepoints) ARE PROVIDED
 if size(cond1)==size(cond2) % Check if arrays of equal size were provided
-    
     delta=cond1-cond2;      %by-subj differences
-    n=sum(~isnan(delta));   %number of participants
-    df=n-1;
-    
+    r=fastcorrcoef(cond1,cond2,'exclude_nan');
     %Correlation between paired conditions
-    r=fastcorrcoef(cond1,cond2,'exclude_nan'); % MATLABs corrcoef is slow... when using this function for whole-brain
-    % analysis it is the main time-consuming step.
-   
-
-% Old and slow:    
-%     for i=1:size(cond1,2)
-%         r_raw=corrcoef(cond1(:,i),cond2(:,i),'rows','complete');
-%         r(1,i)=r_raw(2);
-%     end
-    
-    % Unstandardized mean outcomes
-    mu=nanmean(delta);      %by-study mean difference
-
-    %SD OF DIFFERENCES!
-    sd_diff=nanstd(delta);  
-    se_mu=sd_diff./sqrt(n);  %by-study se (based on SD of differences!)
-    
-    %Standardized outcome 1: Cohen's d
-    %Here there may be a dispute over how sd_within must be defined.
-    % Alternative suggestion by Lakens (2013) ("Cohen's d AV):
-    %sd1=nanstd(cond1);
-    %sd2=nanstd(cond2);
-    %sd_pooled=(sd1+sd2)/2;
-    sd_pooled=sd_diff./sqrt(2.*(1-r));% see:[1], Formula 4.27
-    d=mu./sd_pooled; % see:[1]
-
-    var_d=(1./n+((d.^2)./(2.*n))).*2.*(1-r); %see: [1],4.28  The standard error of the standardized difference is corrected by the intra-subject correlation r
-    se_d= sqrt(var_d) ;
-    
-    %Standardized outcome 2: Hedge's adjusted g
-    J=1-(3./(4.*df-1)); %Approximation to Hedge's correction factor J according to [1]
-    g=d.*J;  % Hedges adjusted g, See: [1] Deeks JJ, Higgins JP. Statistical algorithms in Review Manager 5 on behalf of the Statistical Methods Group of The Cochrane Collaboration. 2010
-    se_g=sqrt(var_d.*J.^2);
-
-    % Standardized mean difference
-
-% VERSION B: COMPUTE IF ONLY ONE A CONTRAST + AN IMPUTED CORRELATION IS
-% PROVIDED (also can used for one-sample estimates if is set to 0)
+    % MATLABs corrcoef is slow... when using this function for whole-brain
+    % analysis it is the main time-consuming step. Thus >> fastcorrcoef
+    % Old and slow verion:    
+    %     for i=1:size(cond1,2)
+    %         r_raw=corrcoef(cond1(:,i),cond2(:,i),'rows','complete');
+    %         r(1,i)=r_raw(2);
+    %     end
 elseif length(cond2)==1 
-       % Unstandardized mean difference
-        delta=cond1;      %here, first column is equal to delta
-        n=sum(~isnan(delta));   %number of participants
-        sd_diff=nanstd(delta);
-        mu=nanmean(delta);
-        se_mu=sd_diff./sqrt(n);
-       %Un-standardized outcome 1: Cohen's d... this is tricky, because no SD of
-       % the basic condition is available
-        r=cond2; %IMPUTED R
-        if r==0
+    delta=cond1;      %if contrasts are entered
+    r= cond2;  % cond2 is IMPUTED
+    if r==0
             %fprintf('WARNING BY withinMetastats.m: r=0 was used >> one-sample test.\n');
+    elseif abs(r)>=1
+            fprintf('WARNING BY withinMetastats.m: r>=1 or r<=-1 was used >> Aborted to avoid nonsense.\n');
+            StudyStat=[];
+            return
         else
             sprintf('WARNING BY withinMetastats.m: r=%0.2f was imputed for this dataset.\n',r);
-        end
-        sd_pooled=sd_diff./sqrt(2.*(1-r)); %see: [1] Borenstein M, Hedges L V, Higgins JPT, Rothstein HR. Effect Sizes Based on Means. Introduction to Meta-Analysis.2009. FORMULA 4.27
-
-        d=mu./sd_pooled; % ATTENTION: Imputed r affects d!!! see: [1] or better Cochrane handbook 16.4.6.2  Standardized mean difference
-        var_d=(1./n+((d.^2)./(2.*n))).*2.*(1-r); %same as above
-        se_d=sqrt(var_d) ; %same as above
-       % Standardized outcome 2: Hedge's adjusted g
-        df=n-1;
-        J=1-(3./(4.*df-1)); %Approximation to Hedge's correction factor J according to [1]
-        g=d.*J;  % Hedges adjusted g, See: [1] Deeks JJ, Higgins JP. Statistical algorithms in Review Manager 5 on behalf of the Statistical Methods Group of The Cochrane Collaboration. 2010
-        se_g=sqrt(var_d.*J.^2);
-        r=ones(size(g)).*(cond2); %Set IMPUTED r as in same format as inputs output!!
+    end
+     r=ones(1,size(cond1,2)).*r; % extend imputed r to all columns
 else
-        fprintf('WARNING BY withinMetastats.m: Vectors entered in and cond1,cond2 were not of equal length.\n')
+        fprintf('WARNING BY withinMetastats.m: Vectors entered in and cond1,cond2 were not of equal length or cond2 was not a scalar.\n')
 end
 
-% Group results in struct to make output shorter and easier to handle
+%% Calculate study stats
+
+n=sum(~isnan(delta));   %number of participants
+df=n-1;
+
+% Unstandardized mean outcomes
+mu=nanmean(delta);      %by-study mean difference
+
+%SD OF DIFFERENCES!
+sd_diff=nanstd(delta);  
+se_mu=sd_diff./sqrt(n);  %by-study se (based on SD of differences!)
+
+%Standardized outcome 1: Cohen's d
+%Here there may be a dispute over how sd_within must be defined.
+% Alternative suggestion by Lakens (2013) ("Cohen's d AV):
+%sd1=nanstd(cond1);
+%sd2=nanstd(cond2);
+%sd_pooled=(sd1+sd2)/2;
+sd_pooled=sd_diff./sqrt(2.*(1-r));% see:[1], Formula 4.27
+d=mu./sd_pooled; % see:[1]
+
+var_d=(1./n+((d.^2)./(2.*n))).*2.*(1-r); %see: [1],4.28  The standard error of the standardized difference is corrected by the intra-subject correlation r
+se_d= sqrt(var_d) ;
+
+%Standardized outcome 2: Hedge's adjusted g
+J=1-(3./(4.*df-1)); %Approximation to Hedge's correction factor J according to [1]
+g=d.*J;  % Hedges adjusted g, See: [1] Deeks JJ, Higgins JP. Statistical algorithms in Review Manager 5 on behalf of the Statistical Methods Group of The Cochrane Collaboration. 2010
+se_g=sqrt(var_d.*J.^2);
+
+%% Guard against outlier results when n's are very small (n=<3)
+% Studies with an n<3 lead to a number of problems: r will be 1 or -1, thus calculation of sd_pooled, and all standardized measures will fail
+% This can happen at imaging analyses when certain voxels are covered by
+% only a fraction of participants from a given study.
+n2small=(n <= 3); 
+
+mu(n2small)=NaN;
+sd_diff(n2small)=NaN;
+sd_pooled(n2small)=NaN;
+se_mu(n2small)=NaN;
+n(n2small)=NaN;
+r(n2small)=NaN;
+d(n2small)=NaN;
+se_d(n2small)=NaN;
+g(n2small)=NaN;
+se_g(n2small)=NaN;
+delta(n2small)=NaN;
+std_delta(:,n2small)=NaN;
+
+%% Put results
+% Group results in struct
 StudyStat.mu=mu;
 StudyStat.sd_diff=sd_diff;
 StudyStat.sd_pooled=sd_pooled;
