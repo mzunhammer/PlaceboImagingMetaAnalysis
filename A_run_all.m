1a1b634727ceff75199bfdbdd0be236765da61ba
Unknown <matthias.zunhammer@uk-essen.de>
2018-02-09 13:11:21 +0100

    Update main

--- A_run_all.m
+++ A_run_all.m
@@ -1,114 +1,113 @@
 %% Script running all analyses of the placebo-meta-analysis,
-% printing out all numerical results for the paper.
 
-%% Import required functions
-cd /Users/matthiaszunhammer/Dropbox/Boulder_Essen/Analysis
-% SPM required
-addpath(genpath('/Users/matthiaszunhammer/Documents/MATLAB/spm12/'))
-% All custom meta-analysis functions required
-addpath(genpath('/Users/matthiaszunhammer/Dropbox/Boulder_Essen/'))
+% SPECIFY MANUALLY: Add analysis to workspace to make all functions available
+analysispath='/Users/matthiaszunhammer/Dropbox/Boulder_Essen/analysis'; % SPECIFY MANUALLY
+addpath(genpath(analysispath));
+% SPECIFY MANUALLY: Path where result figures should be saved
+pubpath='/Users/matthiaszunhammer/Dropbox/Boulder_Essen/Protocol_and_Manuscript/NPS_placebo/NEJM/Figures/';
+% SPECIFY MANUALLY: Path where the data-set rests
+datapath='/Users/matthiaszunhammer/Dropbox/Boulder_Essen/datasets'; %MUST BE ABSOLUTE PATH. SPM and CANlab functions do not deal well with relative paths
 
-%% A Run Imports
-% will create one .mat file in ./Datasets for each study. Each .mat file
-% contains a df (dataframe, table) with paths to images and the respective
-% behavioral/demografic/imaging data
+%% A) import raw data and image paths into data-frame
+% (creates: ../datasets/data_frame.mat)
 
-run('./A_Import/A_Run_All_Single_Imports.m')
-run('./A_Import/B_Collect_All_Data_Tables.m') % Collects all data-tables in one big df
+% FOLDER: ./A_import/
+A_create_study_overview_table(datapath);
+B_run_all_single_imports(datapath);
 
-%% B Create one big matrix holding all image data for (inclusive) analysis
+%% B) define sample
 % and a corresponding df
 
-% Will create a copy of each image with size and resolution of SPM's brainmask
-% [WILL DOUBLE THE SIZE OF ORIGINAL DATA]
-run('./B_Prepro_Images/A_Equalize_Image_Size_and_Mask.m')
+% FOLDER: ./B_define_samples/
+A_run_all_single_condition_summaries(datapath);
+B_select_for_meta_full_sample(datapath);
+C_contrast_placebo_minus_control(datapath,'noimcalc');
+D_contrast_placebo_and_control(datapath,'noimcalc');
+% run('./B_define_samples/E_select_for_meta_conservative_sample.m')
 
-% Will create an un-masked matrix + corresponding df for each study
-% >> only performed when alignment in single images looks suspicions
-% since otherwise the data-matrix is too large
-%run('./B_Prepro_Images/B_Create_Y_Unmasked_SingleStudy.m')
+%% C) preprocess images
 
-% Will create an masked matrix + corresponding df for all data in inclusive analysis
-% [WILL APPROCIMATELY DOUBLE THE SIZE OF ORIGINAL DATA]
-run('./B_Prepro_Images/C_Create_Y.m')
+% FOLDER: ./C_prepro_images/
+A_equalize_image_size_and_mask(datapath,'noimcalc'); % Required for all analyses
+%TODO B_scale_voxel_values_by_sd(datapath)'  % Required for SPM analysis
+%TODO C_equalize_smoothing(datapath) % Optional: Just to check if smoothness affects results
 
 %% Apply all Masks (NPS, MHE, Tissue Probability-Maps, Brain-Masks)
+A_apply_tissuemasks(datapath);      %Calculate tissue-signal levels analog to other masks, add to df, used for outlier screening
+B_apply_NPS(datapath);              %Apply NPS study-wise, add to df, save new df as study_NPS
+C_apply_MHE(datapath);        %Apply MHE (whole brain-analog of the NPS published in Zunhammer et al. 2016, Scientific Reports)
+D_apply_SIIPS(datapath);      %Apply SIIPS mask by Woo 2017, Nature Communications
 
-addpath(genpath('/Users/matthiaszunhammer/Documents/MATLAB/CanlabCore/CanlabCore'))
-run('./C_Apply_NPS/A_Apply_TissueMasks_Single_Study.m') %Calculate relative tissue-signal levels study-wise analog to other masks, add to df
-run('./C_Apply_NPS/B_Apply_NPS_Single_Study.m') %Apply NPS study-wise, add to df, save new df as study_NPS
-run('./C_Apply_NPS/C_Apply_MHE_Single_Study.m') %Apply MHE (whole brain-version of NPS from Zunhammer et al. 2016) study-wise, add to df
-%run('./C_Apply_NPS/D_Apply_PlaceboPredict_Single_Study.m') %Calculate relative ABSOLUTE tissue-signal levels study-wise analog to other masks, add to df
-run('./C_Apply_NPS/E_Collect_All_Data_Tables_NPS.m')            %Collect all study_NPS_MHE_NOBRAIN tables in one big df (./Datasets/AllData_w_NPS_MHE_NOBRAIN.mat)
-rmpath(genpath('/Users/matthiaszunhammer/Documents/MATLAB/CanlabCore/CanlabCore')) % avoid conflict-messages with SPM
-%% Quality control and demographic results
+%% Exploratory Analyses
 
-% Check brain coverage and alignment >> see whole brain analysis
-% Create a global coverage image including all files >> visual coverage check
-% run('./D_Exploratory_Analyses/A_Check_Image_Alignment/A_Check_Coverage.m')
-% % Create a coverage image for each study separately >> visual realignment check
-% run('./D_Exploratory_Analyses/A_Check_Image_Alignment/B_Check_Registrations_single_study.m')
-% % Calculate coverage of NPS based on global coverage image and NPS-mask
-% run('./D_Exploratory_Analyses/A_Check_Image_Alignment/C_Check_Coverage_NPS.m') 
+% Check image alignment
+% FOLDER ./E_exploratory_analyses/A_check_image_alignment
+A_check_coverage_and_alignment(datapath);
+B_check_coverage_NPS();
 
-% Study-level: Plot relative signal intensities for white/gray, csf/gray, nobrain/brain (for painful images)
-% run('./D_Exploratory_Analyses/B_Outlier_Search/A_by_study_tissue_signal_outlier_detection.m') 
-% % Subject-level (within studies): Plot relative signal intensities for white/gray, csf/gray, nobrain/brain (for painful images)
-% run('./D_Exploratory_Analyses/B_Outlier_Search/B_by_subject_tissue_signal_outlier_detection.m') 
-% File in which the excluded subjects (for responder analysis) are defined
-run('./D_Exploratory_Analyses/C_Mark_Excluded_Subjects.m') 
+% Check for outliers
+% FOLDER ./E_exploratory_analyses/B_outlier_search/
+A_by_study_tissue_signal_outlier_detection(datapath);   % Refacturing incomplete: absolute  tissue signal estimates missing.
+B_by_subject_tissue_signal_outlier_detection(datapath); % Refacturing incomplete: absolute  tissue signal estimates missing.
+C_designate_excluded(datapath);                         % Designated outliers by behavior and extreme imaging data
+% Also see report of visual follow-up: Outlier_Subject_Checks.xlsx
 
 % Calculate overall demographic and behavioral results for tabulation
-run('./D_Exploratory_Analyses/E_demographics.m')
+% FOLDER ./E_exploratory_analyses/C_descriptive_results
+D_demographics(datapath);
 
-%Note: Pain ratings were imported from each study as supplied by the
-%investigators. Since scaling for these pain ratings (and especially pain
-%thresholds) differ the identification of participants with very small pain
-%ratings was performed by hand on a single-study basis in the original
-%tables
+%% Generic Inverse Variance (GIV) meta-analysis
+% For NPS paper, main outcomes NPS, pain ratings
 
-%% Meta-analysis NPS paper (Ratings, NPS)
-
-%First run unit-test to assert validity of GIV functions (withinMetastats, betweenMetastats, GIVsummary):
-run('./A_Analysis_GIV_Functions/Check_GIV_Validity.m')
+%First run unit-test to assert validity of GIV functions:
+% FOLDER ./GIV_functions
+validate_GIV();
 
 %Assemble full data-set and inclusive/conservative data-set
-run('./E_NPS_Meta_Analysis_Placebo/A1_Select_All_and_Inclusive.m')
-%Assemble responder data-set
-run('./E_NPS_Meta_Analysis_Placebo/A2_Select_Responder.m')
-
-%Perform all meta-analyses for full data-set
-run('./E_NPS_Meta_Analysis_Placebo/B1_MetaAnalysis_All.m')
-%Perform all meta-analyses for inclusive data-set
-run('./E_NPS_Meta_Analysis_Placebo/B2_MetaAnalysis_Inclusive.m')
-%Perform all meta-analyses for responder data-set
-run('./E_NPS_Meta_Analysis_Placebo/B3_MetaAnalysis_Responder.m')
-
+% FOLDER ./F_NPS_meta_analysis_placebo
+A_meta_analysis_all(datapath,pubpath);
+B_meta_analysis_conservative(datapath,pubpath);
+%C_meta_analysis_responder(datapath,pubpath) %Refacturing incomplete: no responder selection, yet
 % Correlation NPS vs Ratings
-run('./E_NPS_Meta_Analysis_Placebo/C_Meta_Placebo_NPS_vs_ratings.m')
-
-%% Meta-analysis validation paper (Ratings, NPS)
-
-% Validation findings NPS (all inclusive)
-% Pain vs baseline
-run('./F_NPS_Meta_Analysis_Validation/A1_NPS_pain_vs_baseline.m')
-run('./F_NPS_Meta_Analysis_Validation/A2_NPS_pain_vs_baseline_outlier_excluded.m')
-
-% Sequence effects and reliability
-run('./F_NPS_Meta_Analysis_Validation/B1_NPS_sequence_effects.m')
-run('./F_NPS_Meta_Analysis_Validation/B1_NPS_reliability.m')
-
-% High pain vs low pain
-run('./F_NPS_Meta_Analysis_Validation/C1_NPS_hi_vs_lo_pain_all.m')
-run('./F_NPS_Meta_Analysis_Validation/C2_NPS_hi_vs_lo_pain_wo_outlier.m')
-
-% Low pain effects
-run('./F_NPS_Meta_Analysis_Validation/D1_NPS_lo_pain_vs_baseline_all.m')
-
-% Medications effects
-run('./F_NPS_Meta_Analysis_Validation/E1_NPS_medication_effects.m')
-run('./F_NPS_Meta_Analysis_Validation/E2_rating_medication_effects.m')
-
-
+D_meta_placebo_correlation_NPS_vs_ratings(datapath,pubpath);
+E_meta_analysis_all_subregions(datapath,pubpath);
+F_funnel_plot_pain_ratings(datapath,pubpath);
 
 %% Whole brain analysis
+% FOLDER ./G_meta_analysis_whole_brain/GIV
+% there are is also a SPM version, but I've opted for the GIV since SPM
+% analysis is not native to meta-analysis
+
+A_vectorize_all(datapath)
+B_mask_missing_voxels(datapath)
+C_winsorize(datapath)
+
+A_WB_meta_analysis_all(datapath)
+B_WB_meta_analysis_permute_pain(datapath,1000)
+C_WB_meta_analysis_permute_placebo(datapath,1000)
+D_WB_meta_analysis_p_values(datapath)
+
+%% Meta-analysis validation paper (Ratings, NPS)
+% 
+% % Validation findings NPS (all inclusive)
+% % Pain vs baseline
+% run('./F_NPS_Meta_Analysis_Validation/A1_NPS_pain_vs_baseline.m')
+% run('./F_NPS_Meta_Analysis_Validation/A2_NPS_pain_vs_baseline_outlier_excluded.m')
+% 
+% % Sequence effects and reliability
+% run('./F_NPS_Meta_Analysis_Validation/B1_NPS_sequence_effects.m')
+% run('./F_NPS_Meta_Analysis_Validation/B1_NPS_reliability.m')
+% 
+% % High pain vs low pain
+% run('./F_NPS_Meta_Analysis_Validation/C1_NPS_hi_vs_lo_pain_all.m')
+% run('./F_NPS_Meta_Analysis_Validation/C2_NPS_hi_vs_lo_pain_wo_outlier.m')
+% 
+% % Low pain effects
+% run('./F_NPS_Meta_Analysis_Validation/D1_NPS_lo_pain_vs_baseline_all.m')
+% 
+% % Medications effects
+% run('./F_NPS_Meta_Analysis_Validation/E1_NPS_medication_effects.m')
+% run('./F_NPS_Meta_Analysis_Validation/E2_rating_medication_effects.m')
+
+%% 
+
