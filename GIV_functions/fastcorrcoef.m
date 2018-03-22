228f88e45ba0f713f1e4fbddf78025155fa7d3c7
Unknown <matthias.zunhammer@uk-essen.de>
2018-03-22 11:41:49 +0100

    Repair NaN handling in fastcorrcoef

--- GIV_functions/fastcorrcoef.m
+++ GIV_functions/fastcorrcoef.m
@@ -1,17 +1,13 @@
-function r=fastcorrcoef(A,B,exclude_nan)
+function r=fastcorrcoef(A,B,varargin)
     % Calculates pearson's correlation coefficient between two matrices A and B
     % COLUMN-WISE
     % bsxfun speeds up the procedure vastly compared to corrcoef
     % Columns containing NaN return NaN by default
     % Setting the field excluded_nan to true excludes NaNs.
     
-    if ~exist('exclude_nan')
-        exclude_nan=false;
-    end
-    
     if  any([isempty(A),isempty(B)])
        r=[] ; % If A or B is empty return empty instead of nan
-    elseif exclude_nan==1|strcmp(exclude_nan,'exclude_nan') % If nans should be excluded
+    elseif strcmp(varargin,'exclude_nan') % If nans should be excluded
         An=bsxfun(@minus,A,nanmean(A,1)); %%% zero-mean
         Bn=bsxfun(@minus,B,nanmean(B,1)); %%% zero-mean
         An=bsxfun(@times,An,1./realsqrt(nansum(An.^2,1))); %% L2-normalization
@@ -24,6 +20,8 @@ function r=fastcorrcoef(A,B,exclude_nan)
         Bn=bsxfun(@times,Bn,1./realsqrt(sum(Bn.^2,1))); %% L2-normalization
         r=sum(An.*Bn,1);
     end
+    
+    r((sum(~isnan(A))<3)|(sum(~isnan(B))<3)) = NaN;% Even if nans are excluded... there cannot be a correlation without data.
     r=round(r,12); % Fixes possible round-off problems, while preserving NaN: limit r to [-1,1].
 
 end
