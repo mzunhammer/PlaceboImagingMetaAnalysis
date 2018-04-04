8b61c5b82645f94ab952fb1a52ad46811475d7c9
Unknown <matthias.zunhammer@uk-essen.de>
2018-04-04 11:06:07 +0200

    Conservative analysis refactured

---
+++ G_meta_analysis_whole_brain/GIV/B_meta_analysis/A_WB_meta_analysis_placebo_conservative.m
@@ -0,0 +1,31 @@
+function A_WB_meta_analysis_placebo_conservative(datapath)
+%% Meta-Analysis AT WHOLE BRAIN LEVEL USING THE GIV method
+% analog to the meta-analysis of NPS results
+
+load(fullfile(datapath,'vectorized_images_full_masked_10_percent'),'dfv_masked');
+load(fullfile(datapath,'data_frame'),'df');
+
+p = mfilename('fullpath'); %CANlab's apply mask do not like relative paths so this cludge is needed
+[p,~,~]=fileparts(p);
+splitp=strsplit(p,'/');
+whole_brain_path=fullfile(filesep,splitp{1:end-1});
+results_path=fullfile(whole_brain_path,'vectorized_results');
+
+%% Meta-Analysis: Effect of placebo treatment on voxel-by-voxel bold response
+placebo_stats=create_meta_stats_voxels_placebo_conservative(df,dfv_masked);
+
+%% Summarize
+summary_placebo=GIV_summary(placebo_stats,{'g','r_external'});
+
+%% Save
+save(fullfile(results_path,'WB_summary_placebo_conservative.mat'),...
+     'summary_placebo','placebo_stats','-v7.3');
+% Print out n's to check for errors
+n_placebo=vertcat(placebo_stats.n);
+n_placebo_min=nanmin(sum(n_placebo))
+n_placebo_max=nanmax(sum(n_placebo))
+
+n_placebocorr=vertcat(placebo_stats.n_r_external);
+n_placebocorr_min=nanmin(sum(n_placebocorr))
+n_placebocorr_max=nanmax(sum(n_placebocorr))
+end
---
+++ G_meta_analysis_whole_brain/GIV/B_meta_analysis/create_meta_stats_voxels_placebo_conservative.m
@@ -0,0 +1,55 @@
+function stats=create_meta_stats_voxels_placebo_conservative(df,dfv)
+%Preallocate stats for speed
+n_studies=size(df,1);
+n_voxel=sum(dfv.brainmask);
+stats(n_studies).mu=[];
+stats(n_studies).sd_diff=[];
+stats(n_studies).sd_pooled=[];
+stats(n_studies).se_mu=[];
+stats(n_studies).n=[];
+stats(n_studies).r=[];
+stats(n_studies).d=[];
+stats(n_studies).se_d=[];
+stats(n_studies).g=[];
+stats(n_studies).se_g=[];
+stats(n_studies).delta=[];
+stats(n_studies).std_delta=[];
+stats(n_studies).ICC=[];
+stats(n_studies).r_external=[];
+stats(n_studies).n_r_external=[];
+
+%% Voxel-by-voxel bold response change due to placebo condition
+for i=1:size(df,1) % Calculate for all studies except...
+    if ~df.excluded_conservative_sample(i) && df.contrast_imgs_only(i)==0 %...data-sets where only contrasts are available
+        if strcmp(df.study_design{i}, 'within') %Calculate within-subject studies
+           ex=df.subjects{i}.excluded;
+           stats(i)=summarize_within(dfv.pain_placebo{i}(~ex,:),dfv.pain_control{i}(~ex,:));
+        elseif strcmp(df.study_design{i}, 'between') %Calculate between-group studies
+           stats(i)=summarize_between(dfv.pain_placebo{i},dfv.pain_control{i}); % no subjects had to be excluded for between-group studies
+        end        
+    end
+end
+% Calculate for those (within-subject) studies where only pla>con contrasts are available
+conOnly=find(df.contrast_imgs_only==1);
+impu_r=nanmean([stats.r]); % ... impute the mean within-subject study correlation observed in all other studies
+for i=conOnly'
+    if ~df.excluded_conservative_sample(i)
+    ex=df.subjects{i}.excluded;
+    stats(i)=summarize_within(dfv.placebo_minus_control{i}(~ex,:),impu_r); % THE con_img VECTOR INCLUDES PAIN vs BASELINE CONTRAST AND IS NOT TO BE USED USED IN PLACEBO VS CONTROL ANALYSIS
+    end
+end
+
+%% Correlation of behavioral effect and voxel-by-voxel bold response
+for i=1:length(stats)
+    ex=df.subjects{i}.excluded;
+    if ~isempty(stats(i).delta) % necessary as "sum" returns 0 for [] for some stupid reason
+        stats(i).r_external=fastcorrcoef(stats(i).delta,df.GIV_stats_rating(i).delta(~ex,:),'exclude_nan'); % correlate single-subject effect of behavior and voxel signal 
+        stats(i).n_r_external=sum(~(isnan(stats(i).delta)|... % the n for the correlation is the n of subjects showing non-nan values at that particular voxels
+                                         isnan(df.GIV_stats_rating(i).delta(~ex,:)))); % AND non nan-ratings
+    else
+        stats(i).r_external=[];%NaN(1,n_voxel);
+        stats(i).n_r_external=[];%NaN(1,n_voxel);
+    end
+end
+
+end
