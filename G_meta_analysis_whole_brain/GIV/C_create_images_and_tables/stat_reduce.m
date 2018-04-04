b98e4ce8d16edfbfc8d4dd7debf5a1e7ddd8a8e4
Unknown <matthias.zunhammer@uk-essen.de>
2018-04-04 11:06:56 +0200

    Refacture stat_reduce for correct single-subject deltas

--- G_meta_analysis_whole_brain/GIV/C_create_images_and_tables/stat_reduce.m
+++ G_meta_analysis_whole_brain/GIV/C_create_images_and_tables/stat_reduce.m
@@ -1,15 +1,16 @@
 function reduced_stat=stat_reduce(stats,i_voxel)
-
+% Function to pick one value from a GIV-stats struct with multiple
+% results (e.g. voxels) per field
 fields = fieldnames(stats);
 
 reduced_stat=cell2struct(cell(length(stats),length(fields)),fields,2);
 reduced_stat=reduced_stat';
 
 maxvoxels=size(stats(1).(fields{1}),2);
-    for i = 1:numel(fields)
-        for j=1:length(stats)
+    for j=1:length(stats)
+        for i = 1:numel(fields)
             if ~isempty(stats(j).(fields{i}))&&size(stats(j).(fields{i}),2)==maxvoxels
-                reduced_stat(j).(fields{i})=stats(j).(fields{i})(i_voxel);
+                reduced_stat(j).(fields{i})=stats(j).(fields{i})(:,i_voxel);
             else
                 reduced_stat(j).(fields{i})=NaN;
             end
